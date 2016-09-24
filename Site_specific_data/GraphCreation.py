98542194a589d2417ebcb2e21cf2387e70d292a8
Aniket <aniketde@umich.edu>
2016-09-24 09:29:29 -0400

    Big fixed

--- Site_specific_data/GraphCreation.py
+++ Site_specific_data/GraphCreation.py
@@ -10,7 +10,7 @@ from sklearn import preprocessing
 import glob
 
 
-NormalizationFlag = 0 #
+NormalizationFlag = 2 # 0 is normalizing by norm, 1 is scale it, 2 is make it in between [0,1]
 dataset_list = []
 for files in glob.glob("*.csv"):
     dataset_list.append(files)
@@ -22,16 +22,25 @@ for site in range(0,N):
     dataset_file = dataset_list[site]
     dataset = genfromtxt(dataset_file, delimiter=',')
     
-    ## (??) Write normalization
-    if NormalizationFlag == 0:
-        dataset_normalized = preprocessing.normalize(dataset, norm='l2')
-    elif NormalizationFlag == 1:
-        dataset_normalized = preprocessing.scale(dataset)
+    if len(dataset.shape) == 2:
+        datasetX = dataset[:,:-1]
+        datasetID = dataset[:,-1]
+    else: 
+        dataset = dataset[np.newaxis,:]
+        datasetX = dataset[:,:-1]
+        datasetID = dataset[:,-1]
     
+    if NormalizationFlag == 0:
+        dataset_normalized = preprocessing.normalize(datasetX, norm='l2')
+    elif NormalizationFlag == 1:
+        dataset_normalized = preprocessing.scale(datasetX)
+    elif NormalizationFlag == 2:
+        min_max_scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
+        dataset_normalized = min_max_scaler.fit_transform(datasetX)
+        
     dataset_site = 'dataset' + str(site)
     exec (dataset_site + " = dataset_normalized")
-     
-
+    
 
 S = np.zeros([N,N])
 for ii in range(0,N):
@@ -45,5 +54,5 @@ for ii in range(0,N):
         S[ii,jj] = np.linalg.norm(M,ord = 'fro')/(float(M.shape[0])*float(M.shape[1]))
 
 
-print S
+#print S
         
