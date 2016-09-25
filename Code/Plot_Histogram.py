605a174d2bab1116e6b46853ac24de4060836a2e
Aniket <aniketde@umich.edu>
2016-09-24 21:52:52 -0400

    histogram

---
+++ Code/Plot_Histogram.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+Sim1 = np.log(Sim)
+X = Sim1.ravel()
+n, bins, patches = plt.hist(X, 100, facecolor='g', alpha=0.75)
+
+plt.title('Histogram of sim')
+plt.grid(True)
+plt.show()
